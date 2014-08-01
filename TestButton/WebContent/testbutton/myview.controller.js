1910df9356fcea5de4b748b1a2fc2271bc274d4f
DeMiter <dimiter.dimitroff@gmail.com>
2014-08-01 08:26:08 -0400

    Share Test Project

---
+++ TestButton/WebContent/testbutton/myview.controller.js
@@ -0,0 +1,38 @@
+sap.ui.controller("testbutton.myview", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf testbutton.myview
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf testbutton.myview
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf testbutton.myview
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf testbutton.myview
+*/
+//	onExit: function() {
+//
+//	}
+
+});
---
+++ TestButton/WebContent/testbutton/myview.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("testbutton.myview", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf testbutton.myview
+	*/ 
+	getControllerName : function() {
+		return "testbutton.myview";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf testbutton.myview
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
